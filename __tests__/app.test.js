4395a392c20cbf7798ae0ccb9539463cc67ea625
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-30 11:11:43 -0700

    change email to username

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import UserService from '../lib/services/UserService.js';
 
 describe('routes', () => {
   beforeEach(() => {
@@ -12,27 +13,27 @@ describe('routes', () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
       .send({
-        email: 'test@test.com',
+        username: 'spob',
         password: 'password',
         profilePhotoUrl: expect.any(String)
       });
     expect(res.body).toEqual({
       id: '1',
-      email: 'test@test.com',
+      username: 'spob',
       profilePhotoUrl: expect.any(String)
     });
+  });
 
-    it('logsin a user via POST', async () => {
-      const res = await request(app)
-        .post('/api/v1/auth/login')
-        .send({
-          email: 'test@test.com',
-          password: 'password'
-        });
-      expect(res.body).toEqual({
-        id: '1',
-        email: 'test@test.com'
+  it('logsin a user via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'spob',
+        password: 'password'
       });
+    expect(res.body).toEqual({
+      id: '1',
+      username: 'spob'
     });
   });
 });
