d2f9436615fedeb36ca56fc78f9d32746595c64a
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-30 09:27:34 -0700

    failing POST Test (signup user)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,21 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('signs up a user via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'test@test.com'
+    });
   });
 });
